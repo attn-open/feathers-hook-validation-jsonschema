f6fce91cf1ebbea4341e648b4696f30349fcbd6e
Joshua Jabbour <code@joshuajabbour.com>
2016-05-17 16:37:32 -0700

    Add compileSchemaFromUrl and return promises.

--- src/index.js
+++ src/index.js
@@ -2,26 +2,67 @@
 
 import Ajv from 'ajv'
 import errors from 'feathers-errors'
+import fetch from 'node-fetch'
 
-const defaults = {
-  allErrors: true
+export function compileSchema (schema, options = {}) {
+  const defaults = {
+    allErrors: true
+  }
+  const validator = new Promise(function (resolve, reject) {
+    const ajv = Ajv(Object.assign({}, defaults, options))
+    try {
+      resolve(ajv.compile(schema))
+    } catch (error) {
+      reject(error)
+    }
+  })
+
+  return initializeHook(validator)
 }
 
-export default function (schema, options = {}) {
-  options = Object.assign({}, defaults, options)
+export function compileSchemaFromUrl (url, options = {}) {
+  const loadSchema = function (url, callback) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((body) => callback(null, body))
+      .catch((error) => callback(error))
+  }
+  const defaults = {
+    allErrors: true,
+    loadSchema: loadSchema
+  }
+  const validator = new Promise(function (resolve, reject) {
+    const ajv = Ajv(Object.assign({}, defaults, options))
 
-  const ajv = Ajv(options)
-  const validate = ajv.compile(schema)
-
-  return (hook) => {
-    if (hook.data) {
-      hook.params.validated = validate(hook.data)
-      if (!hook.params.validated) {
-        throw new errors.Unprocessable('Invalid request data', {
-          errors: validate.errors
-        })
+    loadSchema(url, function (error, baseSchema) {
+      if (error) {
+        reject(error)
       }
-    }
-    return hook
+      ajv.compileAsync(baseSchema, function (error, compiledValidator) {
+        if (error) {
+          reject(error)
+        }
+        return resolve(compiledValidator)
+      })
+    })
+  })
+
+  return initializeHook(validator)
+}
+
+function initializeHook (validator) {
+  return function (hook) {
+    return validator
+      .then((validate) => {
+        if (hook.data) {
+          hook.params.validated = validate(hook.data)
+          if (!hook.params.validated) {
+            throw new errors.Unprocessable('Invalid request data', {
+              errors: validate.errors
+            })
+          }
+        }
+        return hook
+      })
   }
 }
